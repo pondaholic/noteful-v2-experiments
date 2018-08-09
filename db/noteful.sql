d5cf1ff2cab7204bf467ffc015c32850d5d81c18
Shelly Leung <pontificable@yahoo.com>
2018-08-09 10:46:55 -0700

    tags router added

--- db/noteful.sql
+++ db/noteful.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS notes_tags;
 DROP TABLE IF EXISTS notes;
 DROP TABLE IF EXISTS folders;
+DROP TABLE IF EXISTS tags;
 
 CREATE TABLE folders 
 (
@@ -26,4 +28,25 @@ CREATE TABLE notes
 
 INSERT INTO notes
 	(title, content, folder_id) VALUES
-		('Paradise Lost', 'Epic poem', 100);
+		('Paradise Lost', 'Epic poem', 100),
+		('The Stranger', 'Camus on alienation', 103);
+
+CREATE TABLE tags
+(
+	id serial PRIMARY KEY,
+	name text NOT NULL
+);
+
+CREATE TABLE notes_tags
+(
+	note_id INTEGER NOT NULL REFERENCES notes ON DELETE CASCADE,
+	tag_id INTEGER NOT NULL REFERENCES tags ON DELETE CASCADE
+);
+
+INSERT INTO tags (name) VALUES
+	('My Tag 1'),
+	('My Tag 2'),
+	('My Tag 3');
+
+INSERT INTO notes_tags (note_id, tag_id) VALUES
+	(1, 2);
---
+++ routes/tags.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const knex = require('../knex');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+	knex.select('id', 'name')
+		.from('tags')
+		.then(results => {
+			res.json(results);
+		})
+		.catch(err => next(err));
+});
+
+router.get('/:id', (req, res, next) => {
+	const id = req.params.id;
+
+	knex.first('id', 'name')
+		.from('tags')
+		.where('id', id)
+		.then(results => {
+			res.json(results);
+		})
+		.catch(err => {
+			next(err);
+		});
+});
+
+router.put('/:id', (req, res, next) => {
+	const id = req.params.id;
+
+	const newObj = req.body;
+
+	if(!newObj.name) {
+		const err = new Error('Missing `name` in request body');
+		err.status = 400;
+		return next(err);
+	}
+
+	knex.select('id', 'name')
+		.from('tags')
+		.where('id', id)
+		.update(newObj, ['id', 'name'])
+		.then(results => {
+			res.json(results[0]);
+		})
+		.catch(err => {
+			next(err);
+		});
+});
+
+router.delete('/:id', (req, res, next) => {
+	const id = req.params.id;
+
+	knex('tags')
+		.where('id', id)
+		.del()
+		.then(results =>
+			res.json({status: 204}));
+});
+
+router.post('/', (req, res, next) => {
+	const {name} = req.body;
+
+	if (!name) {
+		const err = new Error('Missing `name` in request body');
+		err.status = 400;
+		return next(err);
+	}
+
+	const newItem = {name};
+
+	knex.insert(newItem)
+		.into('tags')
+		.returning(['id', 'name'])
+		.then(results => {
+			res.json(results[0]);
+		})
+		.catch(err => next(err));
+});
+
+module.exports = router;
